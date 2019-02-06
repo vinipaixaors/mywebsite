df5b7f0fbfe10ce837fa680f86dbf89c3be4b81e
Vinícius Paixão <vinicius.paixaors@gmail.com>
2019-02-06 15:03:09 -0200

    criando footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -9,15 +9,37 @@ class Footer extends React.Component{
     return (
       <>
         <section className={s.footerBg}>
-          <div></div>
-          <div></div>
+          <div className={s.headerBg}></div>
+          <div className={s.contentBg}></div>
+          
           <div className={s.container}>
-            Vinicius
+            <div className={s.wrapper}>
+              <div></div>
+              <div>
+                <h1>
+                  <span>Vinícius</span>
+                  <span>Paixão</span>
+                </h1>
+                <ul>
+                  <li><a href="https://github.com/vinipaixaors/">github</a></li>
+                  <li><a href="https://www.linkedin.com/in/viniciuspaixaors/">linkedin</a></li>
+                  {/* <li><a href="#">youtube</a></li>
+                  <li><a href="#">instagram</a></li> */}
+                </ul>
+                <span className={s.op8}>
+                  © {new Date().getFullYear()} Todos direitos reservados 
+                  - Desenvolvido com
+                  {` `}
+                  <a href="https://www.gatsbyjs.org">Gatsby</a>, por Vinícius Paixão
+                </span>
+              </div>
+            </div>
           </div>
-          © {new Date().getFullYear()} Todos direitos reservados 
-          {/* - Desenvolvido com */}
-          {` `}
-          {/* <a href="https://www.gatsbyjs.org">Gatsby</a>, por Vinícius Paixão */}
+          
+          
+          
+
+
         </section>
       </>
     )
