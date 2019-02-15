ce9fbd0f96aa3da0e4c0c887505d30275015f436
Vinícius Paixão <vinicius.paixaors@gmail.com>
2019-02-15 10:26:45 -0200

    inserção da seção INTRO

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,8 @@ const IndexPage = () => (
               </div>
               <div className={s.contentFooter}>
                 <ul className={s.social}>
-                  <li><a href="https://github.com/vinipaixaors/" className={s.iconGithub}> </a></li>
+                  {/* <li><a href="https://github.com/vinipaixaors/" className={s.iconGithub}> </a></li> */}
+                  <li><a href="https://github.com/vinipaixaors/">Github</a></li>
                   <li><a href="https://www.linkedin.com/in/viniciuspaixaors/">linkedin</a></li>
                   <li><a href="https://profiles.udacity.com/p/10855558614">Udacity</a></li>
                   <li><a href="https://www.instagram.com/viniciuspaixaors/">Instagram</a></li>
@@ -62,29 +63,28 @@ const IndexPage = () => (
           </div>
         </div>
       </section>
-    </div>
 
-    {/* <div>
-      <section className={s.sectionBg}>
+      <section className={s.sections}>
         <div className={s.headerBg}></div>
         <div className={s.contentBg}></div>
-        
         <div className={s.container}>
           <div className={s.wrapper}>
-            <div></div>
-
             <div>
-              <h1>
-                <span>Vinícius</span>
-                <span>Paixão</span>
-              </h1>
-              <p>Desenvolvedor Web &amp; Front-end Expert</p>
-              <p>Now go build something great.</p>
+              <div className={s.bannerImgRight}>
+                <h3>Intro</h3>
+                <span>Um pouco sobre meu perfil.</span>
+              </div>
+            </div>
+            <div>
+              <p className={s.firstParagraph}>Coordenador de tecnologia no Metrópoles, atuo na área de tecnologia a mais de 14 anos. Formado em Design Gráfico e estudante de engenharia de software, conquistou o 24º PRÊMIO CNT DE JORNALISMO com o especial CICLOVIAS EM BUSCA DE UMA CIDADE - pelo Correio Braziliense.</p>
+              <p>Através da Vulpe Desing Studio pude exercer os conhecimento adquiridos em minha graduação em Design Gráfico e trabalhei diretamente com o mercado brasiliense. Atualmente meu foco está associado ao desenvolvimento Front-End e Back-End para aplicações web e mobile.</p>
+              <p>Possuo um perfil comunicativo, flexível, responsável e engajado. Disposto a absorver novos desafios e muito interesse em aprender/estudar novas tecnologias e acompanhar as tendências. </p>
+              {/* <p>Abaixo podemos conferir algumas das principais tarefas desempenhadas na profissão: </p> */}
             </div>
           </div>
         </div>
       </section>
-    </div> */}
+    </div>
     
     
     {/* <Footer /> */}
