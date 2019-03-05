33cddd53d1fdbdd0013273fbbd9c2510b3371cea
Vinícius Paixão <vinicius.paixaors@gmail.com>
2019-03-05 00:31:51 -0300

    ajustando dns-prefetch

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,24 +4,24 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-const React = require(`react`)
+// const React = require(`react`)
 
-exports.onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([
-    <link
-      rel="dns-prefetch"
-      key="dns-prefetch-fonts-google"
-      href="https://fonts.googleapis.com"
-    />,
-    <link
-      rel="dns-prefetch"
-      key="dns-prefetch-google-tagmanager"
-      href="https://www.googletagmanager.com"
-    />,
-    <link
-      rel="dns-prefetch"
-      key="dns-prefetch-google-ana;ytics"
-      href="https://www.google-analytics.com"
-    />,
-  ])
-}
+// exports.onRenderBody = ({ setHeadComponents }) => {
+//   setHeadComponents([
+//     <link
+//       rel="dns-prefetch"
+//       key="dns-prefetch-fonts-google"
+//       href="https://fonts.googleapis.com"
+//     />,
+//     <link
+//       rel="dns-prefetch"
+//       key="dns-prefetch-google-tagmanager"
+//       href="https://www.googletagmanager.com"
+//     />,
+//     <link
+//       rel="dns-prefetch"
+//       key="dns-prefetch-google-ana;ytics"
+//       href="https://www.google-analytics.com"
+//     />,
+//   ])
+// }
