f8737bb20240be57bae870f0c1dc87eff8dbd324
Vinícius Paixão <vinicius.paixaors@gmail.com>
2019-03-18 13:37:35 -0300

    ajuste de theme-color e estrutura

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,6 @@ module.exports = {
     `gatsby-remark-prismjs`,
     `gatsby-remark-copy-linked-files`,
     `gatsby-remark-smartypants`,
-    `gatsby-plugin-feed`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
